e13670b3fcce27bebd820e7f1b567c2a88ee89bd
Stefan Milenković <milenko2204@gmail.com>
2018-06-16 19:54:04 +0200

    Napravljene klase
    Napravljene sve potrebne klase i enumeracije

---
+++ WebProjekat/TaxiSluzba/Models/Automobil.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaxiSluzba.Models
+{
+    public class Automobil
+    {
+        private Vozac vozac;
+        private int godiste;
+        private string registarskaOznaka;
+        private string taxiOznaka;
+        private TipVozila tip;
+
+        #region Properties
+        public Vozac Vozac
+        {
+            get
+            {
+                return vozac;
+            }
+
+            set
+            {
+                vozac = value;
+            }
+        }
+
+        public int Godiste
+        {
+            get
+            {
+                return godiste;
+            }
+
+            set
+            {
+                godiste = value;
+            }
+        }
+
+        public string RegistarskaOznaka
+        {
+            get
+            {
+                return registarskaOznaka;
+            }
+
+            set
+            {
+                registarskaOznaka = value;
+            }
+        }
+
+        public string TaxiOznaka
+        {
+            get
+            {
+                return taxiOznaka;
+            }
+
+            set
+            {
+                taxiOznaka = value;
+            }
+        }
+
+        public TipVozila Tip
+        {
+            get
+            {
+                return tip;
+            }
+
+            set
+            {
+                tip = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ WebProjekat/TaxiSluzba/Models/Korisnik.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaxiSluzba.Models
+{
+    public class Korisnik
+    {
+        private string korisnickoIme;
+        private string lozinka;
+        private string ime;
+        private string prezime;
+        private Pol pol;
+        private string jmbg;
+        private string kontaktTelefon;
+        private string email;
+        private Uloga uloga;
+        private List<Voznja> voznje;
+
+        public Korisnik()
+        {
+            Voznje = new List<Voznja>();
+        }
+
+        #region Properties
+        public string KorisnickoIme
+        {
+            get
+            {
+                return korisnickoIme;
+            }
+
+            set
+            {
+                korisnickoIme = value;
+            }
+        }
+
+        public string Lozinka
+        {
+            get
+            {
+                return lozinka;
+            }
+
+            set
+            {
+                lozinka = value;
+            }
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public Pol Pol
+        {
+            get
+            {
+                return pol;
+            }
+
+            set
+            {
+                pol = value;
+            }
+        }
+
+        public string KontaktTelefon
+        {
+            get
+            {
+                return kontaktTelefon;
+            }
+
+            set
+            {
+                kontaktTelefon = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public Uloga Uloga
+        {
+            get
+            {
+                return uloga;
+            }
+
+            set
+            {
+                uloga = value;
+            }
+        }
+
+        public List<Voznja> Voznje
+        {
+            get
+            {
+                return voznje;
+            }
+
+            set
+            {
+                voznje = value;
+            }
+        }
+        #endregion
+    }
+}
